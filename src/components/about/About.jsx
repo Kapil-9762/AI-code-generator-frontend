e8244fdf4e1c0225ff1a6dd87f57fc4dc1da208a
kapil-9762 <kapilprajapati9762@gmail.com>
2026-07-05 10:25:18 +0530

    changed about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className='container about flex jc-center al-center'>
       <div className="about-container flex f-col ">
         <h1>About Me</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad cupiditate similique voluptatibus quis tempore quam placeat unde, Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolore odit itaque ullam tenetur cumque possimus accusantium dolor molestias iste quam, atque quidem, similique quisquam, alias placeat tempore nam repellendus. lorem sunt autem laborum dignissimos commodi minus iste minima repellat deserunt assumenda molestias cum!</p>
+        <p>AI Powered Code Generator is a software application that uses Artificial Intelligence to generate programming code automatically based on user requirements or text prompts. The system helps developers create code quickly, reduces development time, and improves productivity by generating reusable and accurate code components. It provides an easy-to-use interface for generating frontend, backend, and database-related code efficiently.</p>
       </div>
     </div>
   )
